3f4fa25b7995de3d70a01de191187c3082f7e92b
AbraaoSantana1319 <abraaosantana@gmail.com>
2025-06-24 14:05:39 -0300

    first commit

---
+++ src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { ListaCompras } from "./lista-compras/lista-compras";
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, ListaCompras],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+  protected title = 'ListaCompras';
+}
